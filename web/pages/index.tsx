a0d2e60faeb8b9d6ab494625f9200e5c0fdb3f27
Ties Hoenderdos <tieshoenderdos@gmail.com>
2022-03-14 14:43:20 +0100

    inital commit

---
+++ web/pages/index.tsx
@@ -0,0 +1,20 @@
+import { useUser } from '@auth0/nextjs-auth0';
+
+function Landing(): JSX.Element | any {
+  const { user } = useUser();
+
+  return !user ? (
+    <div className='flex items-center justify-center m-auto max-w-90 w-3/4 h-screen'>
+      <button className='w-1/3 h-96 bg-red-400'>
+        <h1 className='text-center'>Investor</h1>
+      </button>
+      <a href='/api/auth/login' className='w-1/3 h-96 bg-orange-300'>
+        <h1 className='text-center'>Start-up</h1>
+      </a>
+    </div>
+  ) : (
+    window.location.assign('/profile')
+  );
+}
+
+export default Landing;
