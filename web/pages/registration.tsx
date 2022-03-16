d91edb539ade3a9b8d355b49df658d9725ced47b
DesignerTies <tieshoenderdos@gmail.com>
2022-03-16 21:40:20 +0100

    Update registration.tsx

--- web/pages/registration.tsx
+++ web/pages/registration.tsx
@@ -1,4 +1,6 @@
 import { useUser } from "@auth0/nextjs-auth0";
+import { NextPage } from "next";
+import { useEffect, useState, useRef } from "react";
 
 const handleRoute = () => {
   if (typeof window !== "undefined") {
@@ -6,15 +8,48 @@ const handleRoute = () => {
   }
 };
 
-const Registration = () => {
+const formSubmit = async (): Promise<void> => {
+  console.log();
+};
+
+const Registration: NextPage = () => {
   const { user, error, isLoading } = useUser();
+  const [role, setRole] = useState("");
+  const roleChange: any = useRef();
 
   if (isLoading) return <div>Loading</div>;
   if (error) return <div>{error}</div>;
 
   if (user) {
     if (user.email === user.name) {
-      return <div>Je hebt nog geen naam</div>;
+      return (
+        <div>
+          <form
+            action=''
+            onSubmit={() => {
+              formSubmit();
+            }}
+          >
+            <input type='text' name='name' placeholder='name' />
+            <select
+              name='role-select'
+              id=''
+              ref={roleChange}
+              onChange={() => setRole(roleChange.current.value)}
+            >
+              <option value='Start-Up'>Start-Up</option>
+              <option value='Investor'>Investor</option>
+            </select>
+            {role === "Investor" && (
+              <>
+                <input type='text' name='toInvest' placeholder='To invest' />
+                <input type='text' name='company' placeholder='company' />
+              </>
+            )}
+            <input type='submit' />
+          </form>
+        </div>
+      );
     } else {
       window.location.assign("/profile");
       return <div>niks..</div>;
