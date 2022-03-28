6ddf01bd3cb4f226989db86b5a5f1fd12c729514
DesignerTies <tieshoenderdos@gmail.com>
2022-03-28 20:54:23 +0200

    added uploading image functionality

--- web/components/startup-modal.tsx
+++ web/components/startup-modal.tsx
@@ -8,33 +8,75 @@ interface Data {
   imageId?: string;
 }
 
-const handleForm = async (
-  event: any,
-  data: Data,
-  allStartups: any[],
-  clickModal: any
-) => {
-  event.preventDefault();
-  axios
-    .post('/api/v1/startup', {
-      userId: data.authorId,
-      title: data.title,
-      content: data.content,
-      imageId: data.imageId,
-    })
-    .then((response) => {
-      allStartups.unshift(response.data);
-    })
-    .then(clickModal)
-    .catch((error) => {
-      console.log(error);
-    });
-};
-
 const StartupModal = ({ clickModal, userId, allStartups }: any) => {
   const [title, setTitle] = useState<string>('');
   const [content, setContent] = useState<string>('');
   const [imageId, setImageId] = useState<string>();
+  const [imageSrc, setImageSrc] = useState<any>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const handleChange = (changeEvent: any) => {
+    const reader = new FileReader();
+
+    reader.onload = (onLoadEvent) => {
+      setImageSrc(onLoadEvent.target?.result);
+    };
+
+    reader.readAsDataURL(changeEvent.target.files[0]);
+  };
+
+  const handleForm = async (
+    event: any,
+    data: Data,
+    allStartups: any[],
+    clickModal: any
+  ) => {
+    event.preventDefault();
+
+    setIsLoading(true);
+
+    const form = event.target;
+    const thumbImgHTML: any = Array.from(form.elements).find(
+      ({ name }: any) => name === 'thumbnail-img'
+    );
+
+    if (thumbImgHTML.value) {
+      console.log(thumbImgHTML.value);
+      const thumbImg = new FormData();
+      for (const file of thumbImgHTML.files) {
+        thumbImg.append('file', file);
+      }
+      thumbImg.append('upload_preset', 'share-your-idea');
+      // post image to cloudinary
+      const imgData = await fetch(
+        'https://api.cloudinary.com/v1_1/tieshoenderdos-nl/image/upload',
+        {
+          method: 'POST',
+          body: thumbImg,
+        }
+      ).then((r) => r.json());
+
+      data.imageId = imgData.secure_url;
+    }
+
+    // post Startup-Data to database
+    axios
+      .post('/api/v1/startup', {
+        userId: data.authorId,
+        title: data.title,
+        content: data.content,
+        imageId: data.imageId,
+      })
+      .then((response) => {
+        allStartups.unshift(response.data);
+        console.log(allStartups);
+      })
+      .catch((error) => {
+        console.log(error);
+      }); // .then(clickModal)
+
+    setIsLoading(false);
+  };
 
   return (
     <div className='flex items-center justify-center fixed z-1 left-0 top-0 w-full h-full overflow-auto bg-modal'>
@@ -45,38 +87,49 @@ const StartupModal = ({ clickModal, userId, allStartups }: any) => {
         >
           &times;
         </span>
-        <form
-          action=''
-          onSubmit={(e) => {
-            e.preventDefault();
-            const data = {
-              authorId: userId,
-              title,
-              content,
-              imageId,
-            };
-            handleForm(event, data, allStartups, clickModal);
-          }}
-          className='m-auto mt-20 flex flex-col max-w-prose'
-        >
-          <input
-            type='text'
-            name='title'
-            placeholder='Title start-up'
-            className='mb-4 rounded-md focus:border-red-700 text-center'
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          <textarea
-            name='content'
-            cols={30}
-            rows={10}
-            className='rounded-md focus:border-red-600'
-            onChange={(e) => setContent(e.target.value)}
-          ></textarea>
-          <button type='submit' className='hover:cursor-pointer'>
-            Submit
-          </button>
-        </form>
+        {!isLoading && (
+          <form
+            method='post'
+            onSubmit={(e) => {
+              e.preventDefault();
+              const data = {
+                authorId: userId,
+                title,
+                content,
+                imageId,
+              };
+              handleForm(event, data, allStartups, clickModal);
+            }}
+            className='m-auto mt-20 flex flex-col max-w-prose'
+          >
+            <input
+              type='text'
+              name='title'
+              placeholder='Title start-up'
+              className='mb-4 rounded-md focus:border-red-700 text-center'
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <textarea
+              name='content'
+              cols={30}
+              rows={10}
+              className='rounded-md focus:border-red-600'
+              onChange={(e) => setContent(e.target.value)}
+            ></textarea>
+            <input
+              type='file'
+              name='thumbnail-img'
+              accept='image/*'
+              placeholder='Upload je image'
+              onChange={handleChange}
+            />
+            <button type='submit' className='hover:cursor-pointer'>
+              Submit
+            </button>
+            <img src={imageSrc} alt='' />
+          </form>
+        )}
+        {isLoading && <div>Loading...</div>}
       </div>
     </div>
   );
