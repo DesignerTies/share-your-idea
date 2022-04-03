5957bb6084ffaf17e0ca0b4425a62b1c93ae1534
DesignerTies <tieshoenderdos@gmail.com>
2022-04-03 22:33:55 +0200

    error handeling

--- web/pages/feed/[slug].tsx
+++ web/pages/feed/[slug].tsx
@@ -1,13 +1,26 @@
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 import { GetServerSideProps, NextPage } from 'next';
 import prisma from '../../lib/prisma';
+import Image from 'next/image';
 
 const FeedPost: NextPage = (props: any) => {
-  return (
+  return props.authorId ? (
     <>
-      {console.log(props)}
-      <div>Hallo</div>
+      <div>
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
+        {props.imageId && (
+          <Image
+            src={props.imageId}
+            height={300}
+            width={300}
+            alt={'Thumbnail of ' + props.title + ' startup'}
+          />
+        )}
+      </div>
     </>
+  ) : (
+    <div>{props.error}</div>
   );
 };
 
@@ -17,22 +30,28 @@ export const getServerSideProps: GetServerSideProps = withPageAuthRequired({
   async getServerSideProps({ params }: any) {
     const slug = params.slug;
 
-    console.log(slug);
-
-    const startUp = await prisma.idea.findUnique({
+    const startUp = await prisma.idea.findMany({
       where: {
-        title: slug,
+        title: {
+          contains: slug,
+          mode: 'insensitive',
+        },
       },
     });
 
     return {
-      props: {
-        authorId: startUp?.authorId,
-        content: startUp?.content,
-        id: startUp?.id,
-        imageId: startUp?.imageId,
-        title: startUp?.title,
-      },
+      props:
+        startUp.length !== 0
+          ? {
+              authorId: startUp[0]?.authorId,
+              content: startUp[0]?.content,
+              id: startUp[0]?.id,
+              imageId: startUp[0]?.imageId,
+              title: startUp[0]?.title,
+            }
+          : {
+              error: 'Deze startup bestaat niet...',
+            },
     };
   },
 });
